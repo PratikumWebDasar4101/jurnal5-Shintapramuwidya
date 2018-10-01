97b926f04cb78db6ab60f5e95a69a667c77da9b1
Shintapramuwidya <42725343+Shintapramuwidya@users.noreply.github.com>
2018-10-01 10:10:34 +0700

    Update prosesregis.php

--- prosesregis.php
+++ prosesregis.php
@@ -1,7 +1,18 @@
 <?php
+$nim=$_POST['nim'];
+$nama=$_POST['nama'];
+$email=$_POST['email'];
+$komen=$_POST['komen'];
+$jumlah= count(explode(" ", $komen));
 
+echo "<h1> HASIL INPUTAN </h1>";
+echo "NIM: $nim <br /> Nama: $nama <br /> Email: $email <br /> Jumlah Kata: $jumlah <br /> Komentar: $komen  ";
 
-
-
-
+if ($jumlah < 5) {
+	echo "<br><br>kurang atuh";
+}
+else
+{
+die("<br><br>$komen");
+}
 ?>
